ddb729598acbdbe1d621dc1b14856c4ec2d5c17f
ilike3p <39431924+ilike3p@users.noreply.github.com>
2023-01-29 01:43:32 -0800

    update tagjs

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -1,20 +1,44 @@
+// Tag Model
+// Dependencies
 const { Model, DataTypes } = require('sequelize');
 
+// Connect
 const sequelize = require('../config/connection.js');
 
+// Tag Class
 class Tag extends Model {}
 
+// Tag Set UP
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    },  
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: 'tag'
   }
 );
 
 module.exports = Tag;
+
+/* Requirements:
+Tag
+  id
+    - Integer
+    - Doesn't allow null values
+    - Set as primary key
+    - Uses auto increment
+  tag_name
+    - String */

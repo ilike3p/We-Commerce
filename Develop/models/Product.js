89ca5d3ba1d5605c75f4f210c154ef3dc2d1c656
ilike3p <39431924+ilike3p@users.noreply.github.com>
2023-01-29 01:41:57 -0800

    update product

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,14 +1,13 @@
-// Product Tag Model
-// Dependencies
+// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-
+// import our database connection from config.js
 const sequelize = require('../config/connection');
 
-// Product class Tag
-class ProductTag extends Model {}
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model {}
 
-// Product tag set up
-ProductTag.init(
+// set up fields and rules for Product model
+Product.init(
   {
     // define columns
     id: {
@@ -17,17 +16,29 @@ ProductTag.init(
       primaryKey: true,
       autoIncrement: true
     },
-    product_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'product',
-        key: 'id'
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
       }
     },
-    tag_id: {
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: { 
+        isNumeric: true 
+      }
+    },
+    category_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'tag',
+        model: 'Category',
         key: 'id'
       }
     }
@@ -37,23 +48,31 @@ ProductTag.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag'
+    modelName: 'Product'
   }
 );
 
-module.exports = ProductTag;
+module.exports = Product;
 
-/* Checked Requirements:
- ProductTag
+/* Requirements:
+Product
   id
   - Integer
   - Doesn't allow null values
   - Set as primary key
   - Uses auto increment
-  product_id
+  product_name
+  - String
+  - Doesn't allow null values
+  price
+  - Decimal
+  - Doesn't allow null values
+  - Validates that the value is a decimal
+  stock
   - Integer
-  - References the product model's id
-  tag_id
+  - Doesn't allow null values
+  - Set a default value of 10
+  - Validates that the value is numeric
+  category_id
   - Integer
-  - References the tag model's id
-  */
+  - References the category model's id */

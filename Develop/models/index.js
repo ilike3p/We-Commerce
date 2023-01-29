f7d4169c7ec8852dde99c74a7fe6de28d25205b7
ilike3p <39431924+ilike3p@users.noreply.github.com>
2023-01-29 01:38:59 -0800

    update index

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,20 +1,78 @@
-// import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
 
-// Products belongsTo Category
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model {}
 
-// Categories have many Products
+// set up fields and rules for Product model
+Product.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: { 
+        isNumeric: true 
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Category',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Product'
+  }
+);
 
-// Products belongToMany Tags (through ProductTag)
+module.exports = Product;
 
-// Tags belongToMany Products (through ProductTag)
-
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
+/* Requirements:
+Product
+  id
+  - Integer
+  - Doesn't allow null values
+  - Set as primary key
+  - Uses auto increment
+  product_name
+  - String
+  - Doesn't allow null values
+  price
+  - Decimal
+  - Doesn't allow null values
+  - Validates that the value is a decimal
+  stock
+  - Integer
+  - Doesn't allow null values
+  - Set a default value of 10
+  - Validates that the value is numeric
+  category_id
+  - Integer
+  - References the category model's id */
